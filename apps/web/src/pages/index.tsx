f310c13cfb76a51891f7aa9f77902fd894f66f93
Rajeev Rajchal <rajeevrajchal12@gmail.com>
2022-07-24 08:58:10 +0545

    setup tailwind

---
+++ apps/web/src/pages/index.tsx
@@ -0,0 +1,10 @@
+import { Button } from "ui";
+
+export default function Web() {
+  return (
+    <div className="bg-red-200 h-screen w-screen">
+      <p className="text-lg uppercase text-black">hello world</p>
+      <Button />
+    </div>
+  );
+}
